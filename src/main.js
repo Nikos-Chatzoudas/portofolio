e64d09bbc40000214e5fdf45e084bf506c51cf04
Nikos Chatzoudas <126014069+Nikos-Chatzoudas@users.noreply.github.com>
2023-06-07 19:11:30 +0300

    new media file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import '../styles/components/hero.css';
 import '../styles/components/about.css';
 import '../styles/components/contact.css';
 import '../styles/components/projects.css';
-
+import '../styles/components/media.css';
 
 
 import '../styles/components/footer.css';
