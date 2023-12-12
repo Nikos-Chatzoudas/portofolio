abe68f706963b805f169d5cd93315fd43984a893
Nikos Chatzoudas <126014069+Nikos-Chatzoudas@users.noreply.github.com>
2023-12-12 02:31:34 +0200

    global update

--- src/main.js
+++ src/main.js
@@ -6,12 +6,8 @@ import '../styles/components/about.css';
 import '../styles/components/contact.css';
 import '../styles/components/projects.css';
 import '../styles/components/media.css';
-
+import '../styles/components/skills.css';
 
 import '../styles/components/footer.css';
 import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
-
-import scroll from './utils/scroll';
-
-scroll();
