f5e0dca4c6b7cbf126d5bd591c6056b30365b2ef
Nikos Chatzoudas <126014069+Nikos-Chatzoudas@users.noreply.github.com>
2023-12-13 03:28:37 +0200

    madia queries and card lighting

--- src/main.js
+++ src/main.js
@@ -7,7 +7,21 @@ import '../styles/components/contact.css';
 import '../styles/components/projects.css';
 import '../styles/components/media.css';
 import '../styles/components/skills.css';
-
 import '../styles/components/footer.css';
-import '../styles/components/mobile-nav.css';
 import '../styles/utils.css';
+
+/*cards animation*/
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+    const rect = target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+  
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+  
+  // Loop through all .card elements and attach the mousemove event listener
+  document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("mousemove", e => handleOnMouseMove(e));
+  });
